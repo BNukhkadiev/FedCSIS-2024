3c5ccb0625a2c97f03d81a62ef9cf2fddebd859c
FrederikHake <frederik.hake@mail.de>
2024-04-17 21:26:24 +0200

    added imputing and column transformer

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,10 @@
 
 # Preprocessing
 from imblearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import make_column_transformer
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.impute import SimpleImputer
 
 
 def get_pipline(estimator, data):
@@ -14,20 +18,32 @@ def get_pipline(estimator, data):
 
 
 def get_column_tranformer(data):
-    #remove as a first step all NA later on we can try to impute 
-    x = data.dropna()
 
     # numerical values
-    number_cols = data.select_dtypes(include='number')
-
+    numerical_cols = data.select_dtypes(include='number').columns
+    numerical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler()),
+    ])
     #categorical values
-    sector_mapping = {
-    'G1': 1, 'G2': 2, 'G3': 3, 'G4': 4,
-    'G5': 5, 'G6': 6, 'G7': 7, 'G8': 8,
-    'G9': 9, 'G10': 10, 'G11': 11
-    }   
-
-    data['Group'] = data['Group'].map(sector_mapping)
-
+    group_transfromer = GroupTransformer()
     #remove result columns do before pipeline
-    #data = data.drop(columns=['Class','Perform'])
+    return make_column_transformer(
+        (numerical_transformer, numerical_cols),
+        (group_transfromer,['Group'])
+        )
+
+class GroupTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_copy = X.copy()
+        sector_mapping = {
+        'G1': 1, 'G2': 2, 'G3': 3, 'G4': 4,
+        'G5': 5, 'G6': 6, 'G7': 7, 'G8': 8,
+        'G9': 9, 'G10': 10, 'G11': 11
+        }   
+
+        X_copy['Group'] = X_copy['Group'].map(sector_mapping)
+        return X_copy
